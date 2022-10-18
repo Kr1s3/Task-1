06703e1af9abf00c5b006cd8b22893af84443f11
Pini4 <User@WIN-GA1Q70LI9LP>
2022-10-18 19:13:26 +0300

    Добавьте файлы проекта.

---
+++ Task№1/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static double random()
+        {
+            Random rnd = new Random();
+            double rand = rnd.Next(-1000, 1000);
+            return rand / 100;
+        }
+
+        double[] array = new double[10];
+        for (int i = 0; i < 10; i++)
+        {
+            array[i] = random();
+        }
+
+        double temp;
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        Console.WriteLine($"Максимальное число:{Convert.ToString(array[array.Length - 1])}");
+
+        Console.WriteLine($"Минимальное число:{Convert.ToString(array[0])}");
+
+    }
+}
+    
